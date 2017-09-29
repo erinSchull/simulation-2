b58657adbbb47dcbcd90ad71b06d88f280910e63
mhutchison90 <michaelhutchison90@gmail.com>
2017-09-29 13:53:40 -0600

    fixes

--- server/controllers/users_controller.js
+++ server/controllers/users_controller.js
@@ -1,1 +1,12 @@
 //STEP 1 MICHAEL 
+
+module.exports = {
+    register: ( req, res, next ) => {
+      const db = req.app.get('db');
+      const { params, query } = req; 
+
+      db.add_inventory([params.id, params.name, params.price, query.desc])   
+        .then( () => res.status(200).send() )
+        .catch( () => res.status(500).send() );
+    }
+  };
